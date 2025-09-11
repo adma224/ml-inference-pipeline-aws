a9e3c47d6de621cbfb8f12b6476733b1c256fb46
Adrian Murillo <adrianmurilloaraya@gmail.com>
2025-09-10 21:04:51 -0600

    Fixed api 405 bug

--- cdk/stacks/frontend_stack.py
+++ cdk/stacks/frontend_stack.py
@@ -6,93 +6,47 @@ from aws_cdk import (
     aws_ssm as ssm,
     aws_iam as iam,
     CfnOutput,
-    RemovalPolicy
+    RemovalPolicy,
 )
-
-
-from aws_cdk import aws_route53 as route53
-from aws_cdk import aws_certificatemanager as acm
-from aws_cdk import aws_cloudfront as cloudfront
-from aws_cdk.aws_s3_origins import S3Origin
-from aws_cdk import aws_route53_targets as targets
-
-
-
-
 from constructs import Construct
 
 class FrontendStack(Stack):
     def __init__(self, scope: Construct, id: str, generate_fn, ping_fn, vote_fn, **kwargs) -> None:
         super().__init__(scope, id, **kwargs)
 
-        # Create an S3 bucket for static website hosting.
-        frontend_bucket = s3.Bucket(self, "FrontendBucket",
+        # 1) Public website bucket (dev baseline)
+        self.frontend_bucket = s3.Bucket(
+            self, "FrontendBucket",
             website_index_document="index.html",
             website_error_document="error.html",
             public_read_access=True,
             block_public_access=s3.BlockPublicAccess(block_public_policy=False),
             auto_delete_objects=True,
-            removal_policy=RemovalPolicy.DESTROY
+            removal_policy=RemovalPolicy.DESTROY,
         )
-
-        frontend_bucket.add_to_resource_policy(
+        self.frontend_bucket.add_to_resource_policy(
             iam.PolicyStatement(
                 actions=["s3:GetObject"],
-                resources=[f"{frontend_bucket.bucket_arn}/*"],
-                principals=[iam.AnyPrincipal()]
+                resources=[f"{self.frontend_bucket.bucket_arn}/*"],
+                principals=[iam.AnyPrincipal()],
             )
         )
 
-        s3_deployment.BucketDeployment(self, "FrontendDeployment",
+        # 2) Deploy static assets
+        s3_deployment.BucketDeployment(
+            self, "FrontendDeployment",
             sources=[s3_deployment.Source.asset("../web_pages")],
-            destination_bucket=frontend_bucket
+            destination_bucket=self.frontend_bucket,
         )
 
-        # Assume frontend_bucket is already created (you have it)
-
-        # Hosted Zone
-        hosted_zone = route53.HostedZone.from_lookup(
-            self, "HostedZone",
-            domain_name="adrianmurillo.io"
-        )
-
-        # Certificate
-        certificate = acm.Certificate(
-            self, "SiteCertificate",
-            domain_name="adrianmurillo.io",
-            validation=acm.CertificateValidation.from_dns(hosted_zone)
-        )
-
-        # CloudFront Distribution
-        distribution = cloudfront.Distribution(
-            self, "FrontendDistribution",
-            default_root_object="index.html",
-            domain_names=["adrianmurillo.io"],
-            certificate=certificate,
-            default_behavior=cloudfront.BehaviorOptions(
-                origin=S3Origin(frontend_bucket),
-                viewer_protocol_policy=cloudfront.ViewerProtocolPolicy.REDIRECT_TO_HTTPS
-            )
-        )
-
-        # Route53 Alias Record
-        route53.ARecord(
-            self, "AliasRecord",
-            zone=hosted_zone,
-            target=route53.RecordTarget.from_alias(targets.CloudFrontTarget(distribution)),
-            record_name="adrianmurillo.io"
-        )
-
-
-
-        # Define the API Gateway
-        api = apigateway.RestApi(
+        # 3) REST API (proxy + built-in CORS)
+        self.api = apigateway.RestApi(
             self, "MLFrontendApi",
             rest_api_name="MLFrontendApi",
             deploy_options=apigateway.StageOptions(
                 stage_name="prod",
                 throttling_rate_limit=100.0,
-                throttling_burst_limit=200,             
+                throttling_burst_limit=200,
             ),
             default_cors_preflight_options=apigateway.CorsOptions(
                 allow_origins=apigateway.Cors.ALL_ORIGINS,
@@ -101,43 +55,23 @@ class FrontendStack(Stack):
             ),
         )
 
-        def add_lambda_method(resource, method, fn):
-            resource.add_method(method,
-                apigateway.LambdaIntegration(fn, integration_responses=[{
-                    "statusCode": "200",
-                    "responseParameters": {
-                        "method.response.header.Access-Control-Allow-Origin": "'*'",
-                        "method.response.header.Access-Control-Allow-Headers": "'Content-Type'",
-                        "method.response.header.Access-Control-Allow-Methods": "'OPTIONS,POST,GET'"
-                    }
-                }]),
-                method_responses=[{
-                    "statusCode": "200",
-                    "responseParameters": {
-                        "method.response.header.Access-Control-Allow-Origin": True,
-                        "method.response.header.Access-Control-Allow-Headers": True,
-                        "method.response.header.Access-Control-Allow-Methods": True
-                    }
-                }]
-            )
-
-        # Then just attach Lambdas with proxy integration:
-        generate = api.root.add_resource("generate")
+        generate = self.api.root.add_resource("generate")
         generate.add_method("POST", apigateway.LambdaIntegration(generate_fn, proxy=True))
 
-        ping = api.root.add_resource("ping")
+        ping = self.api.root.add_resource("ping")
         ping.add_method("GET", apigateway.LambdaIntegration(ping_fn, proxy=True))
 
-        vote = api.root.add_resource("vote")
+        vote = self.api.root.add_resource("vote")
         vote.add_method("POST", apigateway.LambdaIntegration(vote_fn, proxy=True))
 
-        # Save the API URL to SSM for other stacks or services to reference
-        ssm.StringParameter(self, "ApiUrlParam",
-            string_value=api.url,
-            parameter_name="/ml-pipeline/api/url"
+        # 4) Expose the API base URL for the frontend (SSM)
+        ssm.StringParameter(
+            self, "ApiUrlParam",
+            parameter_name="/ml-pipeline/api/url",
+            string_value=self.api.url,  # e.g., https://<id>.execute-api.<region>.amazonaws.com/prod/
         )
 
-        # Outputs for easy access
-        CfnOutput(self, "FrontendBucketName", value=frontend_bucket.bucket_name)
-        CfnOutput(self, "FrontendBucketWebsiteURL", value=frontend_bucket.bucket_website_url)
-        CfnOutput(self, "ApiGatewayURL", value=api.url)
+        # 5) Outputs
+        CfnOutput(self, "FrontendBucketName", value=self.frontend_bucket.bucket_name)
+        CfnOutput(self, "FrontendBucketWebsiteURL", value=self.frontend_bucket.bucket_website_url)
+        CfnOutput(self, "ApiGatewayURL", value=self.api.url)
--- cdk/stacks/network_stack.py
+++ cdk/stacks/network_stack.py
@@ -4,50 +4,94 @@ from aws_cdk import (
     aws_cloudfront as cloudfront,
     aws_route53 as route53,
     aws_route53_targets as targets,
+    aws_apigateway as apigateway,
     aws_s3 as s3,
-    CfnOutput
+    CfnOutput,
 )
-from aws_cdk.aws_s3_origins import S3Origin
-from constructs import Construct  # not Constructs
+from aws_cdk.aws_cloudfront_origins import S3StaticWebsiteOrigin
+from constructs import Construct
+
 
 class NetworkStack(Stack):
-    def __init__(self, scope: Construct, id: str, frontend_bucket: s3.IBucket, **kwargs):
+    def __init__(
+        self,
+        scope: Construct,
+        id: str,
+        *,
+        frontend_bucket: s3.IBucket,
+        rest_api: apigateway.IRestApi,
+        domain_name: str,
+        **kwargs
+    ):
         super().__init__(scope, id, **kwargs)
 
-        domain_name = "adrianmurillo.io"
+        root_domain = domain_name
+        api_subdomain = f"api.{root_domain}"
 
-        # 1. Look up the hosted zone
-        hosted_zone = route53.HostedZone.from_lookup(
-            self, "HostedZone",
-            domain_name=domain_name
-        )
+        # 1) Public hosted zone (create one; if you already have one, switch to HostedZone.from_lookup)
+        hosted_zone = route53.PublicHostedZone(self, "HostedZone", zone_name=root_domain)
 
-        # 2. Create ACM certificate (must be in us-east-1)
-        certificate = acm.Certificate(
+        # 2) Certificates
+        #    NOTE: For CloudFront, the cert must be in us-east-1. If your app is deployed elsewhere,
+        #    consider creating this certificate in a separate, us-east-1-targeted stack.
+        site_cert = acm.Certificate(
             self, "SiteCertificate",
-            domain_name=domain_name,
-            validation=acm.CertificateValidation.from_dns(hosted_zone)
+            domain_name=root_domain,
+            validation=acm.CertificateValidation.from_dns(hosted_zone),
+        )
+        api_cert = acm.Certificate(
+            self, "ApiCertificate",
+            domain_name=api_subdomain,
+            validation=acm.CertificateValidation.from_dns(hosted_zone),
         )
 
-        # 3. Create CloudFront distribution pointing to S3 bucket
+        # 3) CloudFront distribution for the static site (S3 website origin)
         distribution = cloudfront.Distribution(
             self, "FrontendDistribution",
             default_root_object="index.html",
-            domain_names=[domain_name],
-            certificate=certificate,
+            domain_names=[root_domain],
+            certificate=site_cert,  # <- use the site cert
             default_behavior=cloudfront.BehaviorOptions(
-                origin=S3Origin(frontend_bucket),
-                viewer_protocol_policy=cloudfront.ViewerProtocolPolicy.REDIRECT_TO_HTTPS
-            )
+                origin=S3StaticWebsiteOrigin(frontend_bucket),
+                viewer_protocol_policy=cloudfront.ViewerProtocolPolicy.REDIRECT_TO_HTTPS,
+            ),
         )
 
-        # 4. Create Route 53 alias record pointing to CloudFront
+        # 4) Route53 A-record for the website root -> CloudFront
         route53.ARecord(
-            self, "AliasRecord",
+            self, "SiteAliasRecord",
             zone=hosted_zone,
-            record_name=domain_name,
-            target=route53.RecordTarget.from_alias(targets.CloudFrontTarget(distribution))
+            record_name=root_domain,
+            target=route53.RecordTarget.from_alias(targets.CloudFrontTarget(distribution)),
         )
 
-        # 5. Output the CloudFront domain name
+        # 5) API custom domain: api.<root_domain> -> API Gateway (Regional)
+        api_domain = apigateway.DomainName(
+            self, "ApiCustomDomain",
+            domain_name=api_subdomain,
+            certificate=api_cert,
+            endpoint_type=apigateway.EndpointType.REGIONAL,
+            security_policy=apigateway.SecurityPolicy.TLS_1_2,
+        )
+
+        # Base path mapping: map the custom domain root to the REST API prod stage
+        apigateway.BasePathMapping(
+            self, "ApiBasePathMapping",
+            domain_name=api_domain,
+            rest_api=rest_api,
+            stage=rest_api.deployment_stage,
+        )
+
+        # 6) Route53 A-record for api subdomain -> API Gateway custom domain
+        route53.ARecord(
+            self, "ApiAliasRecord",
+            zone=hosted_zone,
+            record_name=api_subdomain,
+            target=route53.RecordTarget.from_alias(targets.ApiGatewayDomain(api_domain)),
+        )
+
+        # Outputs
         CfnOutput(self, "CloudFrontURL", value=f"https://{distribution.domain_name}")
+        CfnOutput(self, "SiteURL", value=f"https://{root_domain}")
+        CfnOutput(self, "ApiURL", value=f"https://{api_subdomain}")
+
